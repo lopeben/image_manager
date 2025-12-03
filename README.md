4aeb2a162729861b6611f0c5a568731da137a226
lopeben <lopeben@gmail.com>
2025-12-03 10:44:24 +0800

    Rename as filebox, set debug false

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# image_manager
+# filebox
 
 ### User Authentication Credentials Management Guide
 
--- app.py
+++ app.py
@@ -386,4 +386,4 @@ def delete_file(filename):
 
 if __name__ == '__main__':
     load_users()  # Load users at startup
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=False)
